f6c0f692a539168a1b61151f804e15e9ce7ae511
suova <kot.siniy@mail.ru>
2020-10-18 15:49:04 +0300

    add filters, need to add requests

--- src/components/SearchPlace.vue
+++ src/components/SearchPlace.vue
@@ -1,20 +1,27 @@
 <template lang="pug">
-  div.searchPlace
-    SearchField
-    SearchParametrs
+  div.searchPlace_column
+    div.searchPlace
+      SearchField
+      SearchParametrs
+    SearchFilters
 </template>
 
 <script>
 import SearchParametrs from "@/components/SearchParametrs";
 import SearchField from "@/components/SearchField";
+import SearchFilters from "@/components/SearchFilters";
 
 export default {
 name: "SearchPlace",
-  components:{SearchParametrs,SearchField}
+  components:{SearchParametrs,SearchField, SearchFilters}
 }
 </script>
 
 <style scoped>
+.searchPlace_column{
+  display: flex;
+  flex-direction: column;
+}
 .searchPlace{
   display: flex;
   flex-direction: row;
